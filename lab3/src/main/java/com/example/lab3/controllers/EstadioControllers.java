a5ebc339db3396ab9346ee3b98446eb8686299c0
Dylan Bustamante <dylan.bustamante@pucp.edu.pe>
2023-08-31 21:56:50 -0500

    lista de estadios

--- lab3/src/main/java/com/example/lab3/controllers/EstadioControllers.java
+++ lab3/src/main/java/com/example/lab3/controllers/EstadioControllers.java
@@ -1,9 +1,15 @@
 package com.example.lab3.controllers;
 
+import com.example.lab3.entity.Estadio;
+import com.example.lab3.entity.Jugador;
 import com.example.lab3.repository.EstadioRepositorio;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Optional;
 
 @Controller
 public class EstadioControllers {
@@ -20,5 +26,29 @@ public class EstadioControllers {
 
         model.addAttribute("lista", estadioRepositorio.findAll());
         return "/estadios/lista";
+    }
+
+    @GetMapping(value="/estadio/nuevo")
+    public String nuevaDistribuidora(){
+        return "/estadios/nuevo";
+    }
+
+    @PostMapping(value = "/estadio/guardar")
+    public String guardarDistribuidora( Estadio estadio){
+
+        estadioRepositorio.save(estadio);
+        return "redirect:/lab3/estadio/lista";
+    }
+
+    @GetMapping("/estadio/borrar")
+    public String borrar(@RequestParam("id") int id) {
+
+        Optional<Estadio> optional = estadioRepositorio.findById(id);
+
+        if (optional.isPresent()) {
+            estadioRepositorio.deleteById(id);
+        }
+
+        return "redirect:/lab3/estadio/lista";
     }
 }
