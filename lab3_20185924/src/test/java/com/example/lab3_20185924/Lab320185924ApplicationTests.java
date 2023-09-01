35c9bbb7d5ee6761dc4c2e309d81dcd63a17888c
Dylan Bustamante <dylan.bustamante@pucp.edu.pe>
2023-08-31 20:34:13 -0500

    primer commit

---
+++ lab3_20185924/src/test/java/com/example/lab3_20185924/Lab320185924ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab3_20185924;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab320185924ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
